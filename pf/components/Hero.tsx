d0c8c6515cb6b8eb267c025919ecb756c95524a1
CameronDickie <cameronbdickie@gmail.com>
2022-11-12 14:24:15 -0500

    Responsive Changes
    Modified the text and the pfp to be better scaling at smaller sizes.
    Also added in a min-height for the background component

--- pf/components/Hero.tsx
+++ pf/components/Hero.tsx
@@ -27,19 +27,31 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="h-3/4 flex justify-start">
-      <div
-        id="left"
-        className={`${styles.leftHero_start} bg-sky-500 w-2/3 h-full`}
-      ></div>
-      <div
-        id="right"
-        className={`${styles.rightHero_start} bg-rose-500 w-1/3 h-full`}
-      ></div>
-      <img
-        src={pfp.src}
-        className={`${styles.heropfp} translate-y-1/2 w-48 h-48`}
-      ></img>
+    <div className="w-screen h-3/4 min-h-hero relative">
+      <div className="h-full flex justify-start">
+        <div
+          id="left"
+          className={`${styles.leftHero_start} bg-violet-500 w-2/3 h-full`}
+        >
+          <div
+            className={`${styles.heroText} lg:ml-32 md:ml-16 ml-8 lg:w-auto md:w-48 w-32`}
+          >
+            <p className="text-lg text-white font-atkinson_regular">hi, i'm</p>
+            <p className="lg:text-6xl md:text-4xl text-3xl text-rose-500 font-atkinson_bold">
+              Cameron Dickie
+            </p>
+            <p className="text-white md:text-lg text-md font-atkinson_regular">
+              {" "}
+              I strive to balance aesthetics with accessibility.
+            </p>
+          </div>
+        </div>
+        <div
+          id="right"
+          className={`${styles.rightHero_start} bg-rose-500 w-1/3 h-full`}
+        ></div>
+      </div>
+      <img src={pfp.src} className={`${styles.heropfp} w-48 h-48`}></img>
     </div>
   );
 };
--- pf/tailwind.config.js
+++ pf/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    minHeight: {
+      hero: "400px",
+    },
     extend: {
       fontFamily: {
         rubik: ["Rubik Mono One", "sans-serif"],
