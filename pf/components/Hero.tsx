d7acabba79eb163e7e498c20df8ac28158805038
CameronDickie <camerondickie@gmail.com>
2023-06-07 15:56:53 -0400

    Website Simplification
    Simplifying the rest of the website instead of the flashy homepage card

--- pf/components/Hero.tsx
+++ pf/components/Hero.tsx
@@ -1,33 +1,10 @@
 import React from "react";
-import { useEffect } from "react";
 import styles from "../styles/hero.module.css";
 import pfp from "../public/pfp.png";
 
 const Hero = () => {
-  let leftPos = { x: 0, y: 0 };
-  let rightPos = { x: 0, y: 0 };
-  useEffect(() => {
-    /*
-    Sliding animation commented out in here and in the css file... not sure i like the look of it, but could maybe work
-    */
-    // setTimeout(() => {
-    //   let leftElement = document.getElementById("left");
-    //   let rightElement = document.getElementById("right");
-    //   if (leftElement)
-    //     leftElement.className = `${styles.leftHero_middle} bg-sky-500 w-2/3 h-full`;
-    //   if (rightElement)
-    //     rightElement.className = `${styles.rightHero_middle} bg-rose-500 w-1/3 h-full`;
-    //   setTimeout(() => {
-    //     if (leftElement)
-    //       leftElement.className = `${styles.leftHero_end} bg-sky-500 w-2/3 h-full`;
-    //     if (rightElement)
-    //       rightElement.className = `${styles.rightHero_end} bg-rose-500 w-1/3 h-full`;
-    //   }, 250);
-    // }, 350);
-  }, []);
-
   return (
-    <div className="w-screen h-3/4 min-h-hero relative">
+    <div className="w-screen h-3/5 min-h-hero relative">
       <div className="h-full flex justify-start">
         <div
           id="left"
@@ -37,7 +14,7 @@ const Hero = () => {
             className={`${styles.heroText} lg:ml-32 md:ml-16 ml-8 lg:w-auto md:w-48 w-32`}
           >
             <p className="text-lg text-white font-atkinson_regular">
-              hi, i&apos;m
+              hi, I&apos;m
             </p>
             <p className="lg:text-6xl md:text-4xl text-3xl text-rose-500 font-atkinson_bold">
               Cameron Dickie
