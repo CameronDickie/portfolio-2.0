7059b77bf8eaf04ca56cbfbf6c8d7eee9e937910
Cameron Dickie <cameronbdickie@gmail.com>
2023-06-08 15:53:47 -0400

    Adding Background animation for summary
    Added in a css animation for the background on the summary page.
    
    Might decide this to be too tacky after looking at it for too long but
    for now- i like it... maybe I can create a custom one that I can rework
    later but for now this will work

--- pf/components/Summary.tsx
+++ pf/components/Summary.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 
+import styles from "../styles/summary.module.css";
+
 const Summary = () => {
   return (
-    <div className="bg-gray-100 w-screen h-screen flex justify-center items-center">
-      <div className="w-5/6 h-5/6 bg-gray-200 transition ease-in-out hover:-translate-y-1 border-violet-800 border shadow-lg hover:shadow-xl">
-        <h2 className="p-2 font-atkinson_bold text-center text-rose-500 bg-gradient-to-l from-gray-100 via-violet-800 to-violet-800 text-xl">
-          what am I about?
-        </h2>
-        <div className="font-atkinson_regular p-2">
-          antidisestablishmentarianism
+      <div className={`${styles.bgCard} w-screen h-screen flex justify-center items-center`}>
+        <div className="w-5/6 h-5/6 bg-gray-200 transition ease-in-out hover:-translate-y-1 border-violet-800 border shadow-lg hover:shadow-xl">
+          <h2 className="p-2 font-atkinson_bold text-center text-rose-500 bg-gradient-to-l from-gray-100 via-violet-800 to-violet-800 text-xl">
+            what am I about?
+          </h2>
+          <div className="font-atkinson_regular p-2">
+            antidisestablishmentarianism
+          </div>
+          {/* <div className="w-full bg-violet-800 h-0.5"></div> */}
         </div>
-        {/* <div className="w-full bg-violet-800 h-0.5"></div> */}
       </div>
-    </div>
   );
 };
 
